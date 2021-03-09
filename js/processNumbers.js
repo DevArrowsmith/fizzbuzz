ebb1c48848f10dc3fd5685c1be35b12db2408c64
GummiCode <gummicodeblog@gmail.com>
2021-03-09 20:27:32 +0000

    Correct an error in processNumbers: convert passed end value, which will be a string, to a number.

--- js/processNumbers.js
+++ js/processNumbers.js
@@ -1,6 +1,6 @@
 const processNumbers = (start, end, fizz, buzz) => {
     let result = [];
-    for (let i = start; i < end + 1; i++) {
+    for (let i = start; i < parseInt(end) + 1; i++) {
         result.push(checkFizzBuzz(i, fizz, buzz));
     };
     return result;
